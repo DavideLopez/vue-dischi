e1ebf9310bbc846c548adcdc328378cc2bef6a1b
DavideLopez <davidelopez7@gmail.com>
2022-09-07 20:41:42 +0200

    generate card

---
+++ src/components/CardDischi.vue
@@ -0,0 +1,46 @@
+<template class="bg-dark">
+    <div class="card">
+      <div class="card-content">
+        <img :src="poster" alt="">
+        <h3 class="album">{{ title }}</h3>
+        <div class="info-album">
+          <div class="author">{{ author }}</div>
+          <div class="year">{{ year }}</div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      poster: String,
+      title: String,
+      author: String,
+      year: Number,
+    }
+  }
+  </script>
+
+<style lang="scss" scoped>
+
+.card {
+  background-color: #65528D;
+  padding: 15px;
+  border-radius: 0;
+  .card-content {
+    text-align: center;
+    
+    .album {
+      font-size: 14px;
+      color: white;
+      text-transform: uppercase;
+      padding: 20px 0;
+    }
+    .info-album {
+      color: gray;
+      font-size: 16px;
+    } 
+  }
+}
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,52 +1,52 @@
 <template>
-    <div class="hello bg-secondary">
-       <div v-for="(poster, i) in listAlbums" :key="i"> 
-           IO SONO IL CONTENUTO
-          {{poster.i}}
-       </div>
-        
-    </div>
-  </template>
-  
-  <script>
-   import axios from "axios";
+  <div class="container"> 
+    <CardDischi v-for="(disco,i) in ListAlbum" :key="i" 
+      :poster="disco.poster"
+      :title="disco.title"
+      :author="disco.author"
+      :year="disco.year"
+    />
+  </div>
+</template>
 
-  export default {
-    name: 'MainContent',
-    props: {
-      msg: String,
-    },
+<script>
+import axios from 'axios';
+import CardDischi from './CardDischi.vue';
+
+export default {
+    name: "MainComponent",
     data() {
-      return {
-        poster: [],
-        listAlbums: [],
-        title: [],
-        author: [],
-        genre: [],
-
-      }
-      },
-    
+        return {
+            ListAlbum: [],
+        };
+    },
     created() {
         axios
-          .get("https://flynn.boolean.careers/exercises/api/array/music")
-          .then((res) => {
+            .get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then((res) => {
+            this.ListAlbum = res.data.response;
             console.log(res.data);
-            this.listAlbums = res.data;
-          })
-          .catch((err) => {
-            console.log("error", err);
-          })
-          .finally(() => {
-            // console.log("finito");
-          });
-          
+        });
+    },
+    components: { 
+      CardDischi 
     }
-  }
-         
+};
 </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   
+body {
+  
+
+  .container {
+    display: grid;
+    grid-template-columns: repeat(5,1fr);
+    gap: 2rem;
+    max-width: 850px;
+  }
+}
+
+
 </style>
